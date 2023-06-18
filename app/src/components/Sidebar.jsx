43cd920eb768557b88a19662327bdf7ceb8248dd
henzonz <zhenzon26@gmail.com>
2023-06-17 20:32:38 -0700

    Added Videos, VideoCard, and ChannelCard components.

--- app/src/components/Sidebar.jsx
+++ app/src/components/Sidebar.jsx
@@ -2,9 +2,7 @@ import { Stack } from '@mui/material'
 import { categories } from '../utils/constants'
 
 
-const selectedCategory = 'New'
-
-const Sidebar = () => (
+const Sidebar = ({selectedCategory, setSelectedCategory}) => (
     <Stack direction="row"
         sx={{
             overflowY: "auto",
@@ -13,7 +11,9 @@ const Sidebar = () => (
         }}
     >
         {categories.map((category) => (
-            <button className="category-btn"
+            <button
+                className="category-btn"
+                onClick={() => setSelectedCategory(category.name)}
                 style={{
                     background: category.name === selectedCategory && '#FC1503',
                     color: 'white'
--- app/src/components/Videos.jsx
+++ app/src/components/Videos.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
-const Videos = () => {
-  return (
-    <div>Videos</div>
-  )
+import { Stack, Box } from '@mui/material'
+// import { VideoCard, ChannelCard } from './';
+import ChannelCard from './ChannelCard'
+import VideoCard from './VideoCard'
+const Videos = ({ videos }) => {
+    return (
+        <Stack
+            direction="row"
+            flexWrap="wrap"
+            justifyContent="start"
+            gap={2}>
+            {videos.map((item, index) => (
+                <Box key={index}>
+                    {item.id.videoId && <VideoCard video={item} />}
+                    {item.id.channelId && <ChannelCard channelDetail={item} />}
+                </Box>
+            ))}
+        </Stack>
+    )
 }
 
 export default Videos
